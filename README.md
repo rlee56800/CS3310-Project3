00916b330f17de60035a1fd92636e82f5e32467b
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-12-01 23:09:00 -0800

    added text

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CS3310-Project3
+Since all the programs are .py files, they can be opened using any IDE/similar
---
+++ sudoku.py
@@ -0,0 +1,20 @@
+'''
+Task #1 – Sudoku Puzzle Solver                                                                         (35 Points)
+
+Problem Description:
+Sudoku is a number-placement problem in which a partially filled 9x9 grid is filled with digits
+from 1 to 9. The 9x9 grid itself consists of 9 sub-girds of size 3x3. The objective of this
+puzzle is to assign single digits from 1 to 9 in empty cells of this grid such that every row,
+every column and every sub-gird contains exactly one instance of the numbers between1 to 9.
+
+Use backtracking technique to solve a given Sudoku Puzzle.
+
+INPUT:
+
+    Your program must read from an input.txt file which contains a partially filled 9x9 gird
+    of numbers. (To represent empty cells use 0 or -1).
+
+OUTPUT: 
+
+    Print the solved puzzle in format of a 9x9 grid.
+'''
