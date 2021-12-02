2f9cb10e55cfec71c5c62601f1bd4c86183c244b
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-12-01 23:10:01 -0800

    copied format from 01k p2

---
+++ 01Knapsack.py
@@ -0,0 +1,60 @@
+'''
+Task #2 – 0/1 Knapsack Problem                                                  (65 Points)
+
+Use the Best-First  search with Branch-and-Bound algorithm to solve 0/1 knapsack problem.
+
+INPUT:
+    Positive integer W (knapsack capacity) and an input.txt file that contains the weights
+    and profits of n objects. (The first line of this file should contain profits and the
+    second line should include respective weights of n objects; use colon or space to
+    separate values.)
+
+OUTPUT:
+    Visited node, profit, weight, bound, maximum profit and updated Priority queue at each step.
+    An integer maximum profit that is the sum of the profits of an optimal set and objects of
+    that optimal set.
+
+Note: you can use the example explained in the class as your reference.
+'''
+
+capacity = int(input("Please enter value for capacity: "))
+#capacity = 20#7 # testing
+total = 0
+profit = []
+weight = []
+arr = []
+usedProfit = []
+usedWeight = []
+
+with open('input.txt') as f:
+    items = int(next(f)) # first number is amount of items
+    for x in next(f).split(): # first row is profit
+        profit.append(int(x))
+    for x in next(f).split(): # second row is weights
+        weight.append(int(x))
+#print(profit)
+#print(weight)
+'''
+capacity = 7
+total = 0
+items = 4
+profit = [1,4,5,7]
+weight = [1,3,4,5]
+arr = []'''
+
+for i in range(items + 1):
+    arr1 = []
+    for j in range(capacity+1):
+        arr1.append(0)
+    arr.append(arr1)
+
+
+#fill()
+
+#for i in range(len(arr)):
+#    print(arr[i])
+
+print("Max Profit: " + str(arr[items][capacity]))
+print("Items used (profit, then weight):")
+print(usedProfit)
+print(usedWeight)
